2e6a65eed16533af1d57d24ce8f6e29295a047a1
rebelde1231142 <leonelbrenodasilvagithub@gmail.com>
2025-02-17 14:21:25 -0300

    aaaa
    a

--- index.js
+++
@@ -1,5 +0,0 @@
-// filepath: /C:/Users/Aluno/Documents/GitHub/teste/index.js
-import { registerRootComponent } from 'expo';
-import Navigation from './Navigation';
-
-registerRootComponent(Navigation);
